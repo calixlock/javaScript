c8d3a7808c683425ca6ba74e9a23c3da098df719
calixlock <calixlock@naver.com>
2023-06-22 14:55:22 +0900

    load and run figlet's module

---
+++ nodeJs/figlet.js
@@ -0,0 +1,10 @@
+var figlet = require("figlet");
+
+figlet("node.Js START ! !", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
