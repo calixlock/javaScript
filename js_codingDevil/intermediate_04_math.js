ff9cdd710767c6db984639a290cea03b479a172c
calixlock <calixlock@naver.com>
2023-06-29 20:56:43 +0900

    intermediate_math

---
+++ js_codingDevil/intermediate_04_math.js
@@ -0,0 +1,25 @@
+//올림 math
+// 올림
+let n1 = 5.1;
+let n2 = 5.7;
+console.log(`n1 올림 = ${Math.ceil(n1)}`);
+console.log(`n2 올림 = ${Math.ceil(n2)}`);
+console.log(`n1 내림 = ${Math.floor(n1)}`);
+console.log(`n2 내림 = ${Math.floor(n2)}`);
+console.log(`n1 반올림 = ${Math.round(n1)}`);
+console.log(`n2 반올림 = ${Math.round(n2)}`);
+let n3 = 1.25412;
+console.log('소수점 : ',n3.toFixed(2)); //  문자열 반환
+let x = Number('x');
+console.log(x);
+console.log('NaN boolean',!!NaN);
+console.log('NaN type',typeof NaN);
+console.log('random (0~1) : ',Math.random());
+console.log('random (1~101) : ',Math.floor(Math.random()*100)+1);
+console.log(Math.abs(-1));
+console.log(Math.max(-1,213,10,23,2,1,24,));
+console.log(Math.min(-1,213,10,23,2,1,24,))
+console.log(Math.pow(2,3));//2^3
+console.log(Math.sqrt(16)); //16^0.5
+
+
