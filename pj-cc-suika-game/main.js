e89bc5eaa993b28d33c12cbb5193ae6a146e8cf2
calixlock <calixlock@naver.com>
2023-10-31 00:50:18 +0900

    fruit action and keyboard control implement

--- pj-cc-suika-game/main.js
+++ pj-cc-suika-game/main.js
@@ -1,11 +1,13 @@
-import { Bodies, Engine, Render, Runner, World } from "matter-js";
+import { Bodies, Body, Engine, Render, Runner, World } from "matter-js";
+import { FRUITS_BASE } from "./fruits";
+
 // engine / render
 const engine = Engine.create();
 const render = Render.create({
   engine,
   element: document.body,
   options: {
-    wireframes: 0, // 개발자 : 구조 그래픽 단순화 및 확인
+    wireframes: 1, // 그래픽OFF & frameOn
     background: "#f7f4c8",
     width: 620,
     height: 850,
@@ -32,6 +34,7 @@ const background = Bodies.rectangle(310, 820, 620, 60, {
   },
 });
 const topLine = Bodies.rectangle(310, 150, 620, 2, {
+  isSensor: 1, // 감지
   isStatic: 1,
   render: {
     fillStyle: "#e6b143",
@@ -40,6 +43,55 @@ const topLine = Bodies.rectangle(310, 150, 620, 2, {
 
 World.add(world, [leftWall, rightWall, background, topLine]);
 
-// --------------------------------
+//----------------------------------------------------------------
 Render.run(render);
 Runner.run(engine);
+
+let currentBody = null;
+let currentFruit = null;
+let disableAction = 0;
+
+//----------------------------------------------------------------
+function addFruit() {
+  const index = Math.floor(Math.random() * 5); // 0~5 index 생성
+  const fruit = FRUITS_BASE[index];
+  const body = Bodies.circle(300, 50, fruit.radius, {
+    index: index,
+    isSleeping: 1, // 물체 고정
+    render: {
+      sprite: { texture: `${fruit.name}.png` },
+    },
+    restitution: 0.3,
+  });
+  currentBody = body;
+  currentFruit = fruit;
+
+  World.add(world, body);
+}
+window.onkeydown = (event) => {
+  if (disableAction) return;
+  switch (event.code) {
+    case "ArrowLeft":
+      Body.setPosition(currentBody, {
+        x: currentBody.position.x - 10,
+        y: currentBody.position.y,
+      });
+      break;
+    case "ArrowRight":
+      Body.setPosition(currentBody, {
+        x: currentBody.position.x + 10,
+        y: currentBody.position.y,
+      });
+      break;
+    case "ArrowDown":
+      disableAction = 1;
+      currentBody.isSleeping = 0;
+      setTimeout(() => {
+        addFruit();
+        disableAction = 0;
+      }, 1000); //1s delay
+      break;
+  }
+};
+
+addFruit();
