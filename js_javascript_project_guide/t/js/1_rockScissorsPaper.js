ad18e529ae551b6e797ccbbcd711cb8e2a190349
calixlock <calixlock@naver.com>
2024-03-14 15:35:13 +0900

    RSP result implement

--- js_javascript_project_guide/t/js/1_rockScissorsPaper.js
+++ js_javascript_project_guide/t/js/1_rockScissorsPaper.js
@@ -15,6 +15,41 @@ if (userInput !== "rock" && userInput !== "scissors" && userInput !== "paper") {
     comInput = "paper";
   }
 
-  document.querySelector(".userInput").textContent = userInput;
-  document.querySelector(".comInput").textContent = comInput;
+  // 가위바위보 결과
+  function RSP(user, com) {
+    let resultList = ["이겼다", "비겼다", "졌다"];
+    if (user === "rock") {
+      switch (com) {
+        case "rock":
+          return resultList[1];
+        case "paper":
+          return resultList[2];
+        default:
+          return resultList[0];
+      }
+    } else if (user === "scissors") {
+      switch (com) {
+        case "rock":
+          return resultList[2];
+        case "paper":
+          return resultList[0];
+        default:
+          return resultList[1];
+      }
+    } else {
+      switch (com) {
+        case "rock":
+        case "rock":
+          return resultList[0];
+        case "paper":
+          return resultList[1];
+        default:
+          return resultList[2];
+      }
+    }
+  }
+  document.querySelector(".userInput").textContent = "user : " + userInput;
+  document.querySelector(".comInput").textContent = "com : " + comInput;
+  document.querySelector(".result").textContent =
+    "result : " + RSP(userInput, comInput);
 }
