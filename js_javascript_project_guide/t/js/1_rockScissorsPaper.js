89a71e2bafde9b01b4a7b9868f144eb4e9c23dc5
calixlock <calixlock@naver.com>
2024-03-14 15:09:49 +0900

    RSP input implement

--- js_javascript_project_guide/t/js/1_rockScissorsPaper.js
+++ js_javascript_project_guide/t/js/1_rockScissorsPaper.js
@@ -1,14 +1,20 @@
+// 유저 입력
 let userInput = prompt("rock/scissors/paper 중 1개를 입력하세요");
-
-let comInput;
-let rndNum = Math.random();
-if (rndNum < 0.33) {
-  comInput = "rock";
-} else if (rndNum < 0.66) {
-  comInput = "scissors";
+//사용자 입력 검증
+if (userInput !== "rock" && userInput !== "scissors" && userInput !== "paper") {
+  alert("다시 입력하세요");
 } else {
-  comInput = "paper";
+  // 컴퓨터 랜덤 선택
+  let comInput;
+  let rndNum = Math.random();
+  if (rndNum < 0.33) {
+    comInput = "rock";
+  } else if (rndNum < 0.66) {
+    comInput = "scissors";
+  } else {
+    comInput = "paper";
+  }
+
+  document.querySelector(".userInput").textContent = userInput;
+  document.querySelector(".comInput").textContent = comInput;
 }
-// console.log(comInput);
-document.querySelector(".userInput").textContent = userInput;
-document.querySelector(".comInput").textContent = comInput;
