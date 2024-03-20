d6735b092fc904a796415257f62762965930b52e
calixlock <calixlock@naver.com>
2024-03-20 11:37:53 +0900

    pj03 js com score implement update

--- js_javascript_project_guide/project_03_score/js/script.js
+++ js_javascript_project_guide/project_03_score/js/script.js
@@ -2,23 +2,30 @@
 // userSoreElem.innerHTML = "<ch>zero</ch>";
 // let userScoreElem = document.getElementById("user-score");
 // console.log(userScoreElem);
-
-let textElem = document.querySelector("#text");
+let comScore = 0;
+let comScoreElem = document.querySelector("#com-score");
 
 let onComShoot = () => {
+  let textElem = document.querySelector("#text");
+  //슛타입 50%확률로 결정
   let shootType = Math.random() < 0.5 ? 2 : 3;
-  console.log(shootType);
+
   if (shootType === 2) {
+    // 2점슛 성공확률 50%
     if (Math.random() < 0.5) {
       textElem.innerHTML = "COM 2점슛을 성공!";
+      comScore += 2;
     } else {
       textElem.innerHTML = "COM 2점슛을 실패!";
     }
   } else {
+    // 3점 슛 성공확률 33%
     if (Math.random() < 0.33) {
       textElem.innerHTML = "COM 3점슛을 성공!";
+      comScore += 3;
     } else {
       textElem.innerHTML = "COM 3점슛을 실패!";
     }
   }
+  comScoreElem.innerHTML = comScore;
 };
