fa8aa9a6b3ed415b69e2fc4d94d5ead92e8e2e2a
calixlock <calixlock@naver.com>
2024-06-24 15:15:56 +0900

    빈값에는 작동하지 않는 코드 적용

--- js_gymCoding/01_dom-control/02_dynamic-element.js
+++ js_gymCoding/01_dom-control/02_dynamic-element.js
@@ -1,10 +1,31 @@
+let cl = console.log;
 let addBtn = document.getElementById("button");
 let inputBox = document.querySelector(".input");
+// 빈 값 체크 함수
+function checkInput() {
+  if (inputBox.value === "") {
+    alert("빈 값 발생, 다시 입력해주세요.");
+    return false;
+  }
+  return true;
+}
 //btn 기능 부여---------------------------------------
 addBtn.addEventListener("click", () => {
+  if (!checkInput()) return;
   //자식태그 추가---------------------------------------
   let li = document.createElement("li");
   let ul = document.querySelector("ul.list");
   li.textContent = inputBox.value;
   ul.appendChild(li);
+});
+//특정위치에 추가------------------------------
+let addBeforeBtn = document.querySelector("#before");
+addBeforeBtn.addEventListener("click", () => {
+  if (!checkInput()) return;
+  let li = document.createElement("li");
+  let ul = document.querySelector("ul.list");
+  let targetLi = document.querySelector(".target");
+  // 빈값처리
+  li.textContent = inputBox.value;
+  ul.insertBefore(li, targetLi);
 });
