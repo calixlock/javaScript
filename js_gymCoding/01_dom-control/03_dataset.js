a124c41aec6c23222fcd5616369c5d416f8bab21
calixlock <calixlock@naver.com>
2024-06-27 10:37:34 +0900

    js03 dataSet을 이용한 item 선택시 img변경

--- js_gymCoding/01_dom-control/03_dataset.js
+++ js_gymCoding/01_dom-control/03_dataset.js
@@ -1,4 +1,31 @@
 let cl = console.log;
 
-let h2 = document.querySelector("h2");
-cl(h2);
+// let h2 = document.querySelector("h2");
+// cl(h2.dataset.test);
+// h2.dataset.test = "change";
+// cl(h2.dataset.test);
+
+// item에 따른 사진 변경
+let itemList = document.querySelectorAll("li");
+let img = document.querySelector("img");
+let selectedItem = document.querySelector(".selectedItem");
+itemList[0].addEventListener("click", (event) => {
+  cl(event.target.dataset.img);
+  img.setAttribute("src", event.target.dataset.img);
+  selectedItem.textContent = event.target.textContent;
+});
+itemList[1].addEventListener("click", (event) => {
+  cl(event.target.dataset.img);
+  img.setAttribute("src", event.target.dataset.img);
+  selectedItem.textContent = event.target.textContent;
+});
+itemList[2].addEventListener("click", (event) => {
+  cl(event.target.dataset.img);
+  img.setAttribute("src", event.target.dataset.img);
+  selectedItem.textContent = event.target.textContent;
+});
+itemList[3].addEventListener("click", (event) => {
+  cl(event.target.dataset.img);
+  img.setAttribute("src", event.target.dataset.img);
+  selectedItem.textContent = event.target.textContent;
+});
