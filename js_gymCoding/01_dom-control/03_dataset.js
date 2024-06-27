c6541bbf53aa5e67161f3b7d9e85ae3daffc7740
calixlock <calixlock@naver.com>
2024-06-27 10:45:02 +0900

    js03 dataSet : 함수를 이용한 최적화

--- js_gymCoding/01_dom-control/03_dataset.js
+++ js_gymCoding/01_dom-control/03_dataset.js
@@ -1,31 +1,22 @@
-let cl = console.log;
-
+// let cl = console.log;
 // let h2 = document.querySelector("h2");
 // cl(h2.dataset.test);
 // h2.dataset.test = "change";
 // cl(h2.dataset.test);
 
 // item에 따른 사진 변경
+// 태그 선택
 let itemList = document.querySelectorAll("li");
 let img = document.querySelector("img");
 let selectedItem = document.querySelector(".selectedItem");
-itemList[0].addEventListener("click", (event) => {
-  cl(event.target.dataset.img);
+// 함수
+let selectItem = (event) => {
   img.setAttribute("src", event.target.dataset.img);
   selectedItem.textContent = event.target.textContent;
-});
-itemList[1].addEventListener("click", (event) => {
-  cl(event.target.dataset.img);
-  img.setAttribute("src", event.target.dataset.img);
-  selectedItem.textContent = event.target.textContent;
-});
-itemList[2].addEventListener("click", (event) => {
-  cl(event.target.dataset.img);
-  img.setAttribute("src", event.target.dataset.img);
-  selectedItem.textContent = event.target.textContent;
-});
-itemList[3].addEventListener("click", (event) => {
-  cl(event.target.dataset.img);
-  img.setAttribute("src", event.target.dataset.img);
-  selectedItem.textContent = event.target.textContent;
-});
+};
+
+itemList[0].addEventListener("click", selectItem);
+itemList[1].addEventListener("click", selectItem);
+itemList[2].addEventListener("click", selectItem);
+itemList[3].addEventListener("click", selectItem);
+// 함수
