f42380186752591d82c5d7103b5302d0fc09dd60
calixlock <calixlock@naver.com>
2024-06-27 14:51:26 +0900

    js06 event-form img error event

--- js_gymCoding/01_dom-control/06_event-form.js
+++ js_gymCoding/01_dom-control/06_event-form.js
@@ -18,3 +18,11 @@ inputBox.addEventListener("blur", (e) => {
   // blur = focus에서 멀어질때
   console.log(`blur !`);
 });
+
+// image error 감지
+let img = document.querySelector("img");
+img.addEventListener("error", (e) => {
+  console.log("image error 발생");
+  // 실패시 보여줄 이미지 코드
+  e.target.src = "../images/01_spring.jpg";
+});
